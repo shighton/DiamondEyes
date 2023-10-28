f8aebe700eb162010969cb2e781af8f724b8aec0
shighton <dime.bastian@gmail.com>
2023-10-27 22:04:40 -0400

    update 10/27

--- livebot.py
+++ livebot.py
@@ -1,35 +1,43 @@
 # Author: Marty MK (https://www.qmr.ai/cryptocurrency-trading-bot-with-alpaca-in-python/)
+# Author: @huseinzol05 on GitHub
 # Modified by Sabastian Highton
 # Info: Evolution strategy agent (reinforcement learning)
 
 from alpaca_trade_api.rest import REST
 from alpaca.data.timeframe import TimeFrame
-from datetime import datetime, timedelta
-import math
+from datetime import datetime
 import time
 import types
 import numpy as np
 import warnings
+
+'''
+from datetime import timedelta
+import math
+import pandas as pd
+import seaborn as sns
+import sys
 from matplotlib import pyplot as plt
+'''
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
 warnings.simplefilter(action='ignore', category=DeprecationWarning)
 
 BASE_URL = "https://paper-api.alpaca.markets"
-KEY_ID = 'PK5BNLJIZ6TZ40UJ1RUU'
-SECRET_KEY = 'hTje1EKntEueNqMiGE5NXwW2rgvbC0sUw4GLrj3o'
+KEY_ID = 'PKVZ6WQ5B5AHJZPKUZVE'
+SECRET_KEY = 'AtDuRfZ51IO1JfgL9u7ENiMwnyT13hqewtQ3bspG'
 
 # Instantiate REST API Connection
 api = REST(key_id=KEY_ID, secret_key=SECRET_KEY, base_url=BASE_URL)
 
 SYMBOL = ['BTC/USD']
 SYM = 'BTCUSD'
-starting_money = 95000
+starting_money = 100000
 SMA_FAST = 12
 SMA_SLOW = 24
 QTY_PER_TRADE = 1
 
-
+'''
 # Description is given in the article
 def get_pause():
     now = datetime.now()
@@ -37,6 +45,7 @@ def get_pause():
     pause = math.ceil((next_min - now).seconds)
     print(f"Sleep for {pause}")
     return pause
+'''
 
 
 # Same as the function in the random version
@@ -48,7 +57,7 @@ def get_position(symbol):
     return 0
 
 
-def canBuy(symbol):
+def can_buy(symbol):
     val = get_position(symbol)
     snap = api.get_latest_crypto_quotes(SYMBOL)['BTC/USD'].ap
     if val > 0:
@@ -58,20 +67,32 @@ def canBuy(symbol):
     return switch
 
 
-def canSell(symbol):
+def can_sell(symbol):
     val = get_position(symbol)
     return val > QTY_PER_TRADE
 
 
 # Returns a series with the moving average
-def get_sma(series, periods):
+'''def get_sma(series, periods):
     return series.rolling(periods).mean()
 
 
 # Checks whether we should buy (fast ma > slow ma)
 def get_signal(fast, slow):
-    print(f"Fast {fast[-1]}  /  Slow: {slow[-1]}")
+    # print(f"Fast {fast[-1]}  /  Slow: {slow[-1]}")
     return fast[-1] > slow[-1]
+
+
+def bollinger_bands(series: pd.Series, length: int = 20, *,
+                    num_stds: tuple[float, ...] = (2, 0, -2), prefix: str = '') -> pd.DataFrame:
+    # Ref: https://stackoverflow.com/a/74283044/
+    rolling = series.rolling(length)
+    bband0 = rolling.mean()
+    bband_std = rolling.std(ddof=0)
+    df = pd.DataFrame({f'{prefix}{num_std}': (bband0 + (bband_std * num_std)) for num_std in num_stds})
+    sns.lineplot(df)
+    plt.show()
+    return df'''
 
 
 # Get up-to-date 1 minute data from Alpaca and add the moving averages
@@ -80,8 +101,8 @@ def get_bars(symbol):
     yesterday = datetime.fromtimestamp(yesterday_ts).strftime('%Y-%m-%d')
 
     crypto_bars = api.get_crypto_bars(symbol, TimeFrame.Minute, start=yesterday).df
-    crypto_bars[f'sma_fast'] = get_sma(crypto_bars.close, SMA_FAST)
-    crypto_bars[f'sma_slow'] = get_sma(crypto_bars.close, SMA_SLOW)
+    # crypto_bars[f'sma_fast'] = get_sma(crypto_bars.close, SMA_FAST)
+    # crypto_bars[f'sma_slow'] = get_sma(crypto_bars.close, SMA_SLOW)
     return crypto_bars
 
 
@@ -99,8 +120,8 @@ def get_imports():
 
 def get_state(data, end_index, num_considered):
     start_index = end_index - num_considered + 1
-    data_block = data[start_index: end_index + 1] if start_index >= 0 else -start_index * [data[0]] + data[
-                                                                                                      0: end_index + 1]
+    data_block = data[start_index: end_index + 1] if start_index >= 0 else -start_index * [data[0]] + \
+                                                                           data[0: end_index + 1]
     result = []
     for i in range(num_considered - 1):
         result.append(data_block[i + 1] - data_block[i])
@@ -149,10 +170,19 @@ class DeepEvolutionStrategy:
                         / (self.population_size * self.sigma)
                         * np.dot(A.T, rewards).T
                 )
+
+            # When the model is doing poorly for half the intended epochs,
+            # the loop will break to start another attempt increasing efficiency.
+            if i == epoch // 2:
+                if self.reward_function(self.weights) < 0:
+                    break
+
+            print('\rEpoch ' + str(i) + ' out of ' + str(epoch) + ' reward: '
+                  + str(self.reward_function(self.weights)), end='')
             if (i + 1) % print_every == 0:
                 print(
-                    'iter %d. reward: %f'
-                    % (i + 1, self.reward_function(self.weights))
+                    '\riter %d. reward: %f'
+                    % (i + 1, self.reward_function(self.weights)), end=''
                 )
         # print('time taken to train:', time.time() - last_time, 'seconds')
 
@@ -257,7 +287,7 @@ class Agent:
                     buy_units = self.max_buy
                 else:
                     buy_units = buy
-                if close[t] in close[close_length - 1: close_length]:
+                if close[t] in close[close_length - 2:]:
                     buy_switch = True
                 total_buy = buy_units * close[t]
                 money -= total_buy
@@ -276,7 +306,7 @@ class Agent:
                     sell_units = quantity
                 if sell_units < 1:
                     continue
-                if close[t] in close[close_length - 1: close_length]:
+                if close[t] in close[close_length - 2:]:
                     sell_switch = True
                 quantity -= sell_units
                 total_sell = sell_units * close[t]
@@ -307,8 +337,12 @@ class Agent:
         return buy_switch, sell_switch, invest > 0
 
 
-window_size = 5
+# Last x minutes of information in each state
+window_size = 10
+
 step_size = 1
+
+no_action_count = 0
 
 while True:
     # GET DATA
@@ -319,31 +353,45 @@ while True:
 
     model = Model(input_size=window_size, layer_size=500, output_size=3)
     agent = Agent(a_model=model, money=starting_money, max_buy=1, max_sell=1)
-    agent.fit(iterations=500, checkpoint=500)
+    agent.fit(iterations=50, checkpoint=50)
 
     # CHECK POSITIONS
     position = get_position(symbol=SYM)
-    should_buy_sma = get_signal(bars.sma_fast, bars.sma_slow)
-    can_buy = canBuy(SYM)
-    can_sell = canSell(SYM)
+    # should_buy_sma = get_signal(bars.sma_fast, bars.sma_slow)
+    able_buy = can_buy(SYM)
+    able_sell = can_sell(SYM)
     agent_buy, agent_sell, agent_good = agent.buy()
-    print(f"Position: {position} / Can Buy: {can_buy} / RL Buy: {agent_buy} / RL Sell: {agent_sell} / "
-          f"RL Good: {agent_good} / SMA Buy: {should_buy_sma}")
-    if (((((position >= 0) & can_buy) & agent_good) & agent_buy) & should_buy_sma):
-        # WE BUY ONE BITCOIN
-        print("tip toe")
+    # if ((((((position >= 0) & able_buy) & agent_good) & agent_buy) & should_buy_sma) & (agent_sell != True)):
+    if ((((((position >= 0) & able_buy) & agent_good) & agent_buy) & (agent_sell != True))):
+        # print(f"\rPosition: {position} / Can Buy: {able_buy} / RL Buy: {agent_buy} / RL Sell: {agent_sell} / "
+        # f"RL Good: {agent_good} / SMA Buy: {should_buy_sma}")
+        print(f"\rPosition: {position} / Can Buy: {able_buy} / RL Buy: {agent_buy} / RL Sell: {agent_sell} / "
+              f"RL Good: {agent_good}")
         api.submit_order(SYM, qty=QTY_PER_TRADE, side='buy', time_in_force="gtc")
         print(f'Symbol: {SYM} / Side: BUY / Quantity: {QTY_PER_TRADE}')
+        time.sleep(2)  # Give position time to update
         print(f"New Position: {get_position(symbol=SYM)}")
         print("*" * 20, 'buy\n')
-        # time.sleep(get_pause())
-    elif ((((((position > 0) & can_sell) & agent_good) & (agent_buy != True)) & agent_sell) & (should_buy_sma != True)):
-        # WE SELL ONE BITCOIN
-        print("tip toe")
+        no_action_count = 0
+    # elif ((((((position > 0) & able_sell) & agent_good) & (agent_buy != True)) & agent_sell) & (
+    # should_buy_sma != True)):
+    elif (((((position > 0) & able_sell) & agent_good) & (agent_buy != True)) & agent_sell):
+        # print(f"\rPosition: {position} / Can Buy: {able_buy} / RL Buy: {agent_buy} / RL Sell: {agent_sell} / "
+        # f"RL Good: {agent_good} / SMA Buy: {should_buy_sma}")
+        print(f"\rPosition: {position} / Can Buy: {able_buy} / RL Buy: {agent_buy} / RL Sell: {agent_sell} / "
+              f"RL Good: {agent_good}")
         api.submit_order(SYM, qty=QTY_PER_TRADE, side='sell', time_in_force="gtc")
         print(f'Symbol: {SYM} / Side: SELL / Quantity: {QTY_PER_TRADE}')
+        time.sleep(2)  # Give position time to update
         print(f"New Position: {get_position(symbol=SYM)}")
         print("*" * 20, 'sell\n')
-        # time.sleep(get_pause())
+        no_action_count = 0
     else:
-        print("*" * 20, 'no action\n')
+        # print(f"\rPosition: {position} / Can Buy: {able_buy} / RL Buy: {agent_buy} / RL Sell: {agent_sell} / "
+        # f"RL Good: {agent_good} / SMA Buy: {should_buy_sma}", end='')
+        print(f"\rPosition: {position} / Can Buy: {able_buy} / RL Buy: {agent_buy} / RL Sell: {agent_sell} / "
+              f"RL Good: {agent_good}", end='')
+        time.sleep(5)
+        no_action_count += 1
+        print('\r' + 'No action #' + str(no_action_count), end='')
+        time.sleep(2)
