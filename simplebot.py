d89c34d04c2a4ce9e0aa1c54e71c7110066137a2
shighton <dime.bastian@gmail.com>
2023-11-08 17:43:47 -0500

    Added bollinger functions and buy low / sell high logic

--- simplebot.py
+++ simplebot.py
@@ -1,7 +1,4 @@
-# Author: Marty MK (https://www.qmr.ai/cryptocurrency-trading-bot-with-alpaca-in-python/)
-# Author: @huseinzol05 on GitHub
-# Modified by Sabastian Highton
-# Info: Evolution strategy agent (reinforcement learning)
+# Author: Sabastian Highton
 
 from alpaca_trade_api.rest import REST
 from alpaca.data.timeframe import TimeFrame
@@ -12,7 +9,6 @@ import warnings
 import pandas as pd
 import seaborn as sns
 from matplotlib import pyplot as plt
-
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
 warnings.simplefilter(action='ignore', category=DeprecationWarning)
@@ -27,19 +23,9 @@ api = REST(key_id=KEY_ID, secret_key=SECRET_KEY, base_url=BASE_URL)
 SYMBOL = ['BTC/USD']
 SYM = 'BTCUSD'
 starting_money = 100000
-SMA_FAST = 12
-SMA_SLOW = 24
+SMA_FAST = 10
+SMA_SLOW = 20
 QTY_PER_TRADE = 1
-
-'''
-# Description is given in the article
-def get_pause():
-    now = datetime.now()
-    next_min = now.replace(second=0, microsecond=0) + timedelta(minutes=1)
-    pause = math.ceil((next_min - now).seconds)
-    print(f"Sleep for {pause}")
-    return pause
-'''
 
 
 # Same as the function in the random version
@@ -84,9 +70,31 @@ def bollinger_bands(series: pd.Series, length: int = 20, *,
     bband0 = rolling.mean()
     bband_std = rolling.std(ddof=0)
     df = pd.DataFrame({f'{prefix}{num_std}': (bband0 + (bband_std * num_std)) for num_std in num_stds})
-    sns.lineplot(df)
-    plt.show()
+    # sns.lineplot(df)
+    # plt.show()
     return df
+
+
+def over_bought_and_sold(df):
+    oversold = []
+    overbought = []
+
+    for i in range(1, len(df)):
+        oversold.append(df[df.columns[1]].iloc[-i] <= df[df.columns[2]].iloc[-i])
+        overbought.append(df[df.columns[1]].iloc[-i] >= df[df.columns[0]].iloc[-i])
+
+    oversold_df = pd.DataFrame(oversold)
+    overbought_df = pd.DataFrame(overbought)
+
+    o_sold_recent = False
+    o_bought_recent = False
+
+    if oversold_df[len(oversold_df) // 2:].nunique()[0] == 2:
+        o_sold_recent = True
+    if overbought_df[len(overbought_df) // 2:].nunique()[0] == 2:
+        o_bought_recent = True
+
+    return o_sold_recent, o_bought_recent
 
 
 # Get up-to-date 1 minute data from Alpaca and add the moving averages
@@ -113,52 +121,54 @@ def get_imports():
 
 
 no_action_count = 0
+transactions = []
+bars = get_bars(symbol=SYMBOL)
+transactions.append(bars.close.values.tolist()[-1])
 
 while True:
     # GET DATA
     bars = get_bars(symbol=SYMBOL)
     close = bars.close.values.tolist()
-    # print(len(close))
-    close_length = len(close) - 1
+
+    band_df = bollinger_bands(pd.Series(close))
+    o_sold, o_bought = over_bought_and_sold(band_df)
 
     # CHECK POSITIONS
     position = get_position(symbol=SYM)
     should_buy_sma = get_signal(bars.sma_fast, bars.sma_slow)
     able_buy = can_buy(SYM)
     able_sell = can_sell(SYM)
-    # if ((((((position >= 0) & able_buy) & agent_good) & agent_buy) & should_buy_sma) & (agent_sell != True)):
-    if (((position >= 0) & able_buy) & should_buy_sma):
-        # print(f"\rPosition: {position} / Can Buy: {able_buy} / RL Buy: {agent_buy} / RL Sell: {agent_sell} / "
-        # f"RL Good: {agent_good} / SMA Buy: {should_buy_sma}")
-        print(f"\rPosition: {position} / Can Buy: {able_buy} / Can Sell: {able_sell} / SMA Buy: {should_buy_sma}")
+    buy_low = bars.close.values.tolist()[-1] < transactions[-1]
+    sell_high = bars.close.values.tolist()[-1] > transactions[-1]
+
+    if ((((position >= 0) & able_buy) & should_buy_sma) & buy_low):
+        print(f"\rPosition: {position} / Can Buy: {able_buy} / Can Sell: {able_sell} / SMA Buy: {should_buy_sma}"
+              f" / Buy Low: {buy_low} / Sell High: {sell_high}")
         api.submit_order(SYM, qty=QTY_PER_TRADE, side='buy', time_in_force="gtc")
         print(f'Symbol: {SYM} / Side: BUY / Quantity: {QTY_PER_TRADE}')
+        transactions.append(bars.close.values.tolist()[-1])
         time.sleep(2)  # Give position time to update
         print(f"New Position: {get_position(symbol=SYM)}")
         print("*" * 20, 'buy\n')
         no_action_count = 0
-    # elif ((((((position > 0) & able_sell) & agent_good) & (agent_buy != True)) & agent_sell) & (
-    # should_buy_sma != True)):
-    elif (((position >= 0) & able_sell) & (should_buy_sma != True)):
-        # print(f"\rPosition: {position} / Can Buy: {able_buy} / RL Buy: {agent_buy} / RL Sell: {agent_sell} / "
-        # f"RL Good: {agent_good} / SMA Buy: {should_buy_sma}")
-        print(f"\rPosition: {position} / Can Buy: {able_buy} / Can Sell: {able_sell} / SMA Buy: {should_buy_sma}")
+    elif ((((position >= 0) & able_sell) & (should_buy_sma != True)) & sell_high):
+        print(f"\rPosition: {position} / Can Buy: {able_buy} / Can Sell: {able_sell} / SMA Buy: {should_buy_sma}"
+              f" / Buy Low: {buy_low} / Sell High: {sell_high}")
         api.submit_order(SYM, qty=QTY_PER_TRADE, side='sell', time_in_force="gtc")
         print(f'Symbol: {SYM} / Side: SELL / Quantity: {QTY_PER_TRADE}')
+        transactions.pop()
+        if len(transactions) == 0:
+            transactions.append(bars.close.values.tolist()[-1])
         time.sleep(2)  # Give position time to update
         print(f"New Position: {get_position(symbol=SYM)}")
         print("*" * 20, 'sell\n')
         no_action_count = 0
     else:
-        # print(f"\rPosition: {position} / Can Buy: {able_buy} / RL Buy: {agent_buy} / RL Sell: {agent_sell} / "
-        # f"RL Good: {agent_good} / SMA Buy: {should_buy_sma}", end='')
-        print(f"\rPosition: {position} / Can Buy: {able_buy} / Can Sell: {able_sell} / "
-              f"SMA Buy: {should_buy_sma}", end='')
+        print(f"\rPosition: {position} / Can Buy: {able_buy} / Can Sell: {able_sell} / SMA Buy: {should_buy_sma}"
+              f" / Buy Low: {buy_low} / Sell High: {sell_high}", end='')
         time.sleep(5)
         no_action_count += 1
-        print('\r' + 'No action #' + str(no_action_count), end='')
-        time.sleep(2)
         for i in range(50):
-            print('\r' + str(i) + ' / 50', end='')
+            print('\r' + 'No action #' + str(no_action_count) + '. Seconds until next trade: ' + str(50 - i), end='')
             time.sleep(1)
             i += 1
